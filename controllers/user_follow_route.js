b970a37e6ec95973e4dd1481c326c3ae7e2c5a7f
tlatkdgus1 <tlatkdgus1@gmail.com>
2018-01-03 11:50:04 +0900

    git account test

--- controllers/user_follow_route.js
+++ controllers/user_follow_route.js
@@ -9,30 +9,30 @@ var Follow = mongoose.model('UserFollowSchema', db.UserFollowSchema);
 router.post('/set', (req, res) => {
 		User.find({ session: req.headers.session }, (err, user) => {
 				if(err){
-					res.send({'status':err});
+				res.send({'status':err});
 				}else{
 				user = user[0];
 				console.log(user.nickname, req.body.follow_user_nickname);
 				if(user.nickname === req.body.follow_user_nickname){
-					res.send({'status':'you can\'t following yourself'});
+				res.send({'status':'you can\'t following yourself'});
 				}else{
 				User.find({nickname:req.body.follow_user_nickname}, (err,follow)=>{
-				console.log(user[0]);
-				if(user[0] === undefined){
-					res.send({'status':'doesn\'t exit user nickname'});
-				}else{
-				Follow.find({nickname: user.nickname, follow_user_nickname: req.body.follow_user_nickname}, (err, follow) => {
-					if(follow[0]){
-					res.send({'status':'already follow'});
-					}else{
-					Follow.collection.insert({ nickname: user.nickname, follow_user_nickname: req.body.follow_user_nickname }, (err, result) => {
-						if(!err){
-						res.send(result);
-						}
-						});}	
-				});}
+						console.log(user[0]);
+						if(user[0] === undefined){
+						res.send({'status':'doesn\'t exit user nickname'});
+						}else{
+						Follow.find({nickname: user.nickname, follow_user_nickname: req.body.follow_user_nickname}, (err, follow) => {
+								if(follow[0]){
+								res.send({'status':'already follow'});
+								}else{
+								Follow.collection.insert({ nickname: user.nickname, follow_user_nickname: req.body.follow_user_nickname }, (err, result) => {
+										if(!err){
+										res.send(result);
+										}
+										});}	
+								});}
 
-				})};}
+						})};}
 		});
 });
 
@@ -47,11 +47,11 @@ router.get('/follow/:nickname', (req, res) => {
 
 router.get('/follow_count/:nickname', (req,res) => {
 		Follow.count({nickname: req.params.nickname}, function(err,count){
-		if(!err){
-			res.send({'count':count});
-		}		
-	});
-});
+				if(!err){
+				res.send({'count':count});
+				}		
+				});
+		});
 
 router.get('/follower/:nickname', (req,res) => { 
 		Follow.find({ follow_user_nickname : req.params.nickname }, (err, follow) => {
@@ -61,28 +61,28 @@ router.get('/follower/:nickname', (req,res) => {
 				});
 		});
 router.get('/follower_count/:nickname', (req,res) => {
-                Follow.count({ follow_user_nickname: req.params.nickname}, function(err,count){
-                if(!err){
-                        res.send({'count':count});
-                }
-        });
-});
+		Follow.count({ follow_user_nickname: req.params.nickname}, function(err,count){
+				if(!err){
+				res.send({'count':count});
+				}
+				});
+		});
 
 router.get('/count/:nickname', (req,res) => {
-                Follow.count({ nickname: req.params.nickname }, (err, follow_count) => {
-                        if(!err){
+		Follow.count({ nickname: req.params.nickname }, (err, follow_count) => {
+				if(!err){
 				Follow.count({ follow_user_nickname: req.params.nickname}, (err, follower_count) => {
-                		if(!err){
-                        	res.send({'follow_count':follow_count, 'follwer_count':follower_count});
-                		}});
-                        }
-                });
-});
+						if(!err){
+						res.send({'follow_count':follow_count, 'follwer_count':follower_count});
+						}});
+				}
+				});
+		});
 
 router.post('/unfollow', (req,res) => {
 		User.find({ session: req.headers.session }, (err, user) => {
 				if(err){
-					res.send({'status':err});
+				res.send({'status':err});
 				}else{
 				user = user[0];
 				Follow.remove({ nickname : user.nickname, follow_user_nickname : req.body.unfollow_user_nickname }, function(err) {
@@ -95,7 +95,7 @@ router.post('/unfollow', (req,res) => {
 						});
 				}
 				});
-		
+
 
 		});
 
